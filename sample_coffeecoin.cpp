176dc4dc22dab32be9dccd771be3db6ce9cc1ad3
Troshin Anton <98951019+TroshinAnton@users.noreply.github.com>
2022-03-28 13:07:09 +0300

    Create sample_coffeecoin.cpp

---
+++ sample_coffeecoin.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define slv   \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define endl '\n'
+
+template <class T>
+inline void in(vector<T> &v)
+{
+    for (auto &i : v)
+        cin >> i;
+}
+void solve()
+{
+   
+    return;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    slv
+    {
+        solve();
+        cout << endl;
+    }
+}

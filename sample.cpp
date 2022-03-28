552142183505c8939065f3af0a6ad74cbf22abf4
Troshin Anton <98951019+TroshinAnton@users.noreply.github.com>
2022-03-28 11:30:12 +0300

    Create sample.cpp

---
+++ sample.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define hotel trivago
+#define vi vector<int>
+#define pii pair<int, int>
+#define fi first
+#define se second
+#define int long long
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+template<class T>
+istream&operator>>(istream&in,vector<T>&a)
+{for(auto&i:a)in>>i;return in;}template
+<class T>ostream&operator<<(ostream&out,
+vector<T>&a){for(auto&i:a)out<<i<<' ';return out;}
+
+
+void solve() {
+   
+}
+
+signed main(void) {
+    ios_base::sync_with_stdio(0);
+    cout.tie(0); cin.tie(0);
+#ifdef troshin20
+    freopen(".in", "r", stdin);
+    freopen(".out", "w", stdout);
+#endif
+    int t; cin >> t;
+    while(t--)solve();
+    return -0;
+}

e4c43237f8a1010d16aeac6483e8cc2d4277a631
Troshin Anton <98951019+TroshinAnton@users.noreply.github.com>
2022-04-12 16:01:54 +0300

    шаблон тов Вани

---
+++ karginux_sample.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+#define debug(x) cerr<<#x<<": "<<x<<'\n'
+using namespace std;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifdef karginux
+    freopen("i.log", "r", stdin);
+    freopen("o.log", "w", stdout);
+#endif
+    cout << "Hello";
+}

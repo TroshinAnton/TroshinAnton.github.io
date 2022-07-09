59517404b107bb4b6b00d81674632f09a642e64e
Troshin Anton <98951019+TroshinAnton@users.noreply.github.com>
2022-07-09 15:15:54 +0300

    аххаахахахахахахах даша

---
+++ sample_capybara_.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+#ifdef LOCAL
+#define DEBUG(...) {__VA_ARGS__}
+#else
+#define DEBUG(...)
+#endif
+
+#define ENDL "\n"
+#define forn(i, s, f) for (int i = (s); i < (f); ++i)
+
+void solve() {
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+//    cin >> t;
+    forn(i, 0, t) {
+        solve();
+    }
+    return 0;
+}
